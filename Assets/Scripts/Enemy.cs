898ff678222d246978e87039bc173a3a7c19a2e3
KrisD789 <krisdnay.sa.66@ubu.ac.th>
2025-09-04 17:29:39 +0700

    Enemy_ai

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Transform player;   // ผู้เล่น
+    public float speed = 3f;   // ความเร็วศัตรู
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            // เดินเข้าหาผู้เล่น
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                player.position,
+                speed * Time.deltaTime
+            );
+
+            // หันหน้าไปหาผู้เล่น
+            Vector3 direction = (player.position - transform.position).normalized;
+            transform.forward = direction;
+        }
+    }
+}
