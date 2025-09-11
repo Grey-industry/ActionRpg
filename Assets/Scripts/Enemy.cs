43144ec48450cc59b61cf19e93c720b12a90cb83
Tonamu0407 <tonnam.pu.66@ubu.ac.th>
2025-09-11 15:09:09 +0700

    SAO

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //linkStarto
     }
 
     // Update is called once per frame
