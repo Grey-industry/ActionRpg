f8b0ebd2e8b5dd3f8a93b7c7abfb328c54749d12
Rittipong <rittipong.ta.66@ubu.ac.th>
2025-09-25 16:00:50 +0700

    UpdateHpBar&playerAttack

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,36 +1,64 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class SlimeChasePlayer : MonoBehaviour
 {
+    NavMeshAgent agent;
+    public GameObject player;
 
-    public Transform player;   // ผู้เล่น
-    public float speed = 3f;   // ความเร็วศัตรู
+    public float stopDistance = 1f;
+    public float detectionRange = 10f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Animator anim;
+    bool isPlayerInRange = false;
+    bool hasReachePlayer = false;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (player != null)
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        isPlayerInRange = distance <= detectionRange;
+        if (isPlayerInRange)
         {
-            // เดินเข้าหาผู้เล่น
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                player.position,
-                speed * Time.deltaTime
-            );
-
-            // หันหน้าไปหาผู้เล่น
-            Vector3 direction = (player.position - transform.position).normalized;
-            transform.forward = direction;
+            if (distance <= stopDistance)
+            {
+                StopMoving();
+                hasReachePlayer = true;
+            }
+            else
+            {
+                MoveToPlayer();
+                hasReachePlayer = false;
+            }
         }
-    } 
+        else
+        {
+            StopMoving();
+        }
+
+
+    }
+    void MoveToPlayer()
+    {
+        if (agent.isActiveAndEnabled)
+        {
+            agent.SetDestination(player.transform.position);
+            hasReachePlayer = false;
+        }
+
+    }
+    void StopMoving()
+    {
+        if (agent.isActiveAndEnabled)
+        {
+            agent.ResetPath();
+            hasReachePlayer = true;
+        }
+    }
 }
-
-
-
-
